4a6f4b597876d5ae676fa9b55ec52c9d29a2359d
Mishkin <9mihail@gmail.com>
2022-09-28 18:46:45 +0300

    added tournament frames and layout

--- app/src/main/java/space/dlsunity/arctour/MainActivity.kt
+++ app/src/main/java/space/dlsunity/arctour/MainActivity.kt
@@ -1,14 +1,11 @@
 package space.dlsunity.arctour
 
 import androidx.fragment.app.FragmentManager
-import space.dlsunity.arctour.domain.services.AppService
 import space.dlsunity.arctour.presenter.base.BaseActivity
 import space.dlsunity.arctour.presenter.screens.errors.ErrorModel
 import space.dlsunity.arctour.utils.auxiliary.FragmentCloseInterface
 
 class MainActivity : BaseActivity(R.layout.activity_main), FragmentCloseInterface {
-
-    lateinit var mChatService: AppService
 
     override fun handlerProgress(visibility: Boolean) {
     }
